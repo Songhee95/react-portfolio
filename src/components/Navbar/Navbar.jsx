d7e3f2d0f32c3d263af8bba60cc369d492d7223d
songhee <thdgml5537@gmail.com>
2021-01-25 00:39:30 -0500

    mobile nav menu auto close function added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ function Navbar() {
     const [clicked, setClicked] = useState(false);
     
     const handleClick = () =>setClicked(!clicked);
+    const closeMenu = ()=> setClicked(false);
 
     return (
         <>
@@ -13,11 +14,11 @@ function Navbar() {
             <nav className="navbar navbar-dark bg-dark">
                 <div className="container-fluid">
                     <div className="head-icons">
-                        <Link className="navbar-brand" to="/">Songhee Yim</Link>
-                        <Link to="/github" target="_blank" className="navbar-brand">
+                        <Link className="navbar-brand" to="/" onClick={closeMenu}>Songhee Yim</Link>
+                        <Link to="/github" target="_blank" className="navbar-brand" onClick={closeMenu}>
                             <i className="fab fa-github github"></i>
                         </Link>
-                        <Link to="/linkedin" target="_blank" className="navbar-brand">
+                        <Link to="/linkedin" target="_blank" className="navbar-brand" onClick={closeMenu}>
                             <i className="fab fa-linkedin linkedin"></i>
                         </Link>
                     </div>
@@ -28,18 +29,18 @@ function Navbar() {
                     {/* List of menu items */}
                     <div className={clicked ? "nav-menu active" : "nav-menu"}>
                         <li className="nav-item">
-                            <NavLink className="nav-links" to="/aboutme">About Me</NavLink>
+                            <NavLink className="nav-links" to="/aboutme" onClick={closeMenu}>About Me</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-links " to="/portfolio">Portfolio</NavLink>
+                            <NavLink className="nav-links " to="/portfolio" onClick={closeMenu}>Portfolio</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a href="./files/resume_Songhee.pdf" target="_blank" download className="nav-links">
+                            <a href="./files/resume_Songhee.pdf" target="_blank" download className="nav-links" onClick={closeMenu}>
                                 <i className="fas fa-download"></i> RESUME
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a href="mailto:thdgml5537@gmail.com" className="nav-links">
+                            <a href="mailto:thdgml5537@gmail.com" className="nav-links" onClick={closeMenu}>
                                 <i className="fas fa-paper-plane"></i> HIRE ME
                             </a>
                         </li>
