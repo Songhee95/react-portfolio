406533b6b3bccef404866e6f4dfc2ee1ad71460a
songhee <thdgml5537@gmail.com>
2021-01-24 21:29:46 -0500

    portfolio project list set with 3 projects

--- src/components/Projects/projectLists.js
+++ src/components/Projects/projectLists.js
@@ -18,6 +18,24 @@ const projectLists = [
     key: 1,
   },
   {
+    title: "Resume Builder",
+    image: "../images/resume_builder.PNG",
+    description:
+      "Resume builder for Developers, Prevent scrambled issue of resume template. Create your account, create resume and manage your resume.",
+    github: "https://github.com/Songhee95/resume-builder",
+    deploy: "https://damp-tundra-89141.herokuapp.com/",
+    icon: [
+      "devicon-css3-plain-wordmark devicon",
+      "devicon-express-original devicon",
+      "devicon-nodejs-plain-wordmark devicon",
+      "devicon-heroku-plain-wordmark devicon",
+      "devicon-javascript-plain devicon",
+      "devicon-mysql-plain-wordmark devicon",
+      "devicon-handlebars-plain-wordmark devicon",
+    ],
+    key: 1,
+  },
+  {
     title: "Movie Chews",
     image: "../images/movie-chew.PNG",
     description:
