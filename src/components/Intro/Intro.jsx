df67862be1176515aacde31b10fd15b55b164bb1
songhee <thdgml5537@gmail.com>
2021-01-25 15:34:19 -0500

    add animation styling on the intro page

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -6,12 +6,14 @@ function Intro() {
     return (
         <div>
             <div className='intro-container'>
-                <img src={process.env.PUBLIC_URL + '/images/songhee.png'} alt='songhee'></img>
-                <p>&lt;Hello my="name-is"&gt;</p>
-                <h1>SONGHEE YIM</h1>
-                <h2>FULL STACK DEVELOPER</h2>
-                <p>&lt;I like="Debugging"&gt;&lt;/I&gt;</p>
-                <p>&lt;/Hello&gt;</p>
+                <img className="intro-pic fadeOut" src={process.env.PUBLIC_URL + '/images/1.jpg'} alt='songhee'></img>
+                <div className="intro-context fadeIn">
+                    <p>&lt;Hello my="name-is"&gt;</p>
+                    <h1>SONGHEE YIM</h1>
+                    <h2>FULL STACK DEVELOPER</h2>
+                    <p>&lt;I like="Debugging"&gt;&lt;/I&gt;</p>
+                    <p>&lt;/Hello&gt;</p>
+                </div>
             </div>
             <About />
         </div>
